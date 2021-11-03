1b8233481bfa4c3e80d2d8972c9a0b9d5db50a33
ederp <epmutley@gmail.com>
2021-11-03 18:13:02 -0300

    Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 
 function App() {
   return (
-    <div className="App">
-          <h1>
+      <div className="App">
+          <h1 className="text-primary">
               Olá mundo!
            </h1>
     </div>
