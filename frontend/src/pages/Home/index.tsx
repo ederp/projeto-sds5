b3905f39dcfe77587e80931170da3fca7b37dca1
ederp <epmutley@gmail.com>
2021-11-05 20:59:41 -0300

    Env var 4 - Resolvendo problema netlify

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,24 +1,24 @@
-import NavBar from "components/NavBar";
-import Footer from "components/Footer";
 import { Link } from "react-router-dom";
+import Footer from "../../components/Footer";
+import NavBar from "../../components/NavBar";
 
 const Home = () => {
     return (
         <>
             <NavBar />
-        <div className="container">
-            <div className="jumbotron">
-                <h1 className="display-4">DSVendas</h1>
-                <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
-                <hr />
-                <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">DSVendas</h1>
+                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <hr />
+                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
                     <Link className="btn btn-primary btn btn-lg" to="/dashboard">
                         Acessar Dashboard
                     </Link>
-             </div>
+                </div>
             </div>
             <Footer />
-            </>
+        </>
     );
 }
 
